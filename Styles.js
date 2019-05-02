3834c92e8ee46c42508d66b45b78d42093d6594b
Neil Kimmett <neilkimmett@gmail.com>
2019-05-01 23:18:56 -0400

    Add prettier, make a bit prettier
    - added prettier to autoformat the code
    - played with the splash screen a bit make it a lil prettier

--- Styles.js
+++ Styles.js
@@ -1,41 +1,47 @@
-import {
-  StyleSheet
-} from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const ErrorStyle = StyleSheet.create({
   style: {
-    color: 'red'
-  }
+    color: "red",
+  },
 });
 
 export const ButtonContainerStyle = StyleSheet.create({
   style: {
-    position: 'absolute',
+    position: "absolute",
     flex: 1,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    bottom: '40%',
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    bottom: "45%",
     padding: 0,
   },
   bottom: {
     flex: 1,
-    position: 'absolute',
-    width: '100%',
+    position: "absolute",
+    width: "100%",
     bottom: 0,
-    padding: 0
-  }
+    padding: 0,
+  },
 });
 
+export const colors = {
+  orange: "#ec682c",
+  white: "#eeeeee",
+};
+
 export const ButtonStyle = StyleSheet.create({
-  style: {
-    backgroundColor: '#ec682caa',
-    alignItems: 'center',
-    width: '100%'
+  primary: {
+    backgroundColor: colors.orange,
   },
-  fill: {
-    backgroundColor: '#ec682cFF',
-    height: 55,
-    width: '100%'
-  }
+  outline: {
+    borderColor: colors.orange,
+    backgroundColor: colors.white,
+  },
+  whiteText: {
+    color: colors.white,
+  },
+  orangeText: {
+    color: colors.orange,
+  },
 });
