f3f9b3e4a88d9ea5cdc7c845fb62fd5738f62da9
Denis Erashov <thewhiteunicorn99@gmail.com>
2020-12-15 15:03:36 +0300

    Fix safe area on ios and android

--- Styles.js
+++ Styles.js
@@ -47,7 +47,7 @@ export const colors = {
 export const globalStyles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    paddingHorizontal: 5,
+    paddingLeft: 5,
     paddingVertical: 5,
   },
 });
