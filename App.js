3e2b3ddbbb3d1d19000215bbdd44e411a50ef5b9
Denis Erashov <thewhiteunicorn99@gmail.com>
2021-01-12 17:18:43 +0300

    Integratied Sentry

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ import { Notifications } from "expo";
 import { AppContainer } from "./Navigation";
 import { api } from "./Api";
 import {SafeAreaProvider} from "react-native-safe-area-context/src/index";
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://b430cf73db3e4ddcb167de15d97ac9c9@o503310.ingest.sentry.io/5588270',
+  enableInExpoDevelopment: true,
+  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+});
+
+Sentry.Native.captureException(new Error('Oops!'))
 
 export default class App extends React.Component {
   constructor(props) {
