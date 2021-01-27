18c157a9f15d048a99f9a7b488820a9a3e604f99
Denis Erashov <thewhiteunicorn99@gmail.com>
2021-01-27 17:23:31 +0300

    AsyncStorage bug on image upload fix & add video feature disabled

--- Api.js
+++ Api.js
@@ -345,28 +345,18 @@ export const precincts = () => {
 
 export const reverseGeocode = location => {
   // console.log("reverse geocode", location);
+  console.log('LOCATION', location.lat.toFixed(4), location.lng.toFixed(4));
   if (!location || !location.lat || !location.lng) {
     return Promise.reject(
       `illegal location ${location == null ? null : JSON.stringify(location)}`
     );
   }
-  const key = `location.${location.lat}.${location.lng}`;
-  return AsyncStorage.getItem(key).then(item => {
-    if (item) {
-      // console.log("found geo");
-      return JSON.parse(item);
-    }
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDiBYFqZLwPsNkMbRNqr1_63h-w9fcZNVM&latlng=${location.lat.toFixed(
-      4
-    )},${location.lng.toFixed(4)}&rankby=distance`;
-    return fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        // console.log("reverse geocoded!");
-        AsyncStorage.setItem(key, JSON.stringify(data));
-        return data;
-      });
-  });
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDiBYFqZLwPsNkMbRNqr1_63h-w9fcZNVM&latlng=${location.lat},${location.lng}&result_type=street_address`;
+  return fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      return data;
+    });
 };
 
 export const api = {
