60433cf15f998e6fdcbe7056a94bed2232cfaf28
Sashka <night@durdom.com>
2023-09-01 15:12:17 -0400

    Fix crash on 'reports'

--- Submissions.js
+++ Submissions.js
@@ -119,7 +119,6 @@ export default class Submissions extends React.Component {
     api
       .reports(this.state.filter)
       .then(res => {
-        console.log('report res', res);
         this.setState({ refreshing: false, error: undefined });
         const addressMap = res.data.addresses.reduce((map, x) => {
           map[x.id] = x;
