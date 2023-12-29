483777dae59751166509a5e96076630167f73e22
nochkin <github@durdom.com>
2023-12-29 00:47:04 -0500

    Profile: use hooks

--- Profile.js
+++ Profile.js
@@ -6,6 +6,7 @@ import {
     Text,
     TouchableOpacity, TouchableOpacityComponent, View,
 } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 import { Input, Icon } from "react-native-elements";
 import LogoTitle from "./LogoTitle";
 import { ErrorStyle, globalStyles } from "./Styles";
@@ -15,9 +16,17 @@ import FloatingMainButton from "./FloatingMainButton";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Button } from "react-native-web";
 import { StackActions } from "@react-navigation/native";
+import { useAuth } from "./AuthProvider";
 
-export default class Profile extends React.Component {
-    static navigationOptions = ({ navigation }) => {
+export default function Profile() {
+    const [editable, setEditable] = React.useState(false);
+    const [userData, setUserData] = React.useState({});
+    const [errors, setErrors] = React.useState({});
+
+    const navigation = useNavigation();
+    const auth = useAuth();
+
+    const navigationOptions = ({ navigation }) => {
         return {
             headerTitle: <LogoTitle title={"PROFILE"} />,
             headerRight: (
@@ -42,211 +51,177 @@ export default class Profile extends React.Component {
         };
     };
 
-    logoutPressed = () => {
-        signOut().then(() => {
-            // this.props.navigation.navigate("Login");
-            this.props.navigation.dispatch(StackActions.replace('SignedOut', { key: 'Login' }));
+    const logoutPressed = () => {
+        auth.logout().then((result) => {
+            console.log('sign out', result);
         });
     };
 
-    editPressed = () => {
-        const { editable } = this.state;
-        this.setState({ editable: !editable });
+    const editPressed = () => {
+        setEditable(!editable);
         if (editable) {
-            this.setData(this.state.user);
+            // this.setData(user);
         }
         const editMessage = editable ? "EDIT" : "CANCEL";
-        this.props.navigation.setParams({
+        navigation.setParams({
             editText: editMessage
         });
     };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            editable: false
-        };
+    const setData = (user) => {
+        setUserData(user);
     }
 
-    setData(user) {
-        this.setState({
-            user,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            phone: user.phone,
-            email: user.email
-        });
+    const onChange = (field, value) => {
+        const user = { ...userData };
+        user[field] = value;
+        setUserData(user);
     }
 
-    componentDidMount() {
-        const { navigation } = this.props;
-
-        isSignedIn().then(user => {
-            console.log('profile', user);
-            if (user) {
-                this.setData(user);
-            } else {
-                this.logoutPressed();
-            }
-        });
-        navigation.setParams({
-            logoutPressed: this.logoutPressed,
-            editPressed: this.editPressed,
-            editText: "EDIT"
-        });
-    }
-
-    onChange = field => value => {
-        this.setState({ [field]: value });
-    }
-
-    onFirstNameBlur() {
+    const onFirstNameBlur = () => {
         let firstNameError = null;
-        if (this.state.firstName.length == 0) {
+        if (!userData.firstName.length) {
             firstNameError = "First Name required";
         } else {
             firstNameError = null;
         }
-        this.setState({ firstNameError });
+        setErrors({ ...errors, ...{ firstNameError } });
     }
 
-    onLastNameBlur() {
+    const onLastNameBlur = () => {
         let lastNameError = null;
-        if (this.state.lastName.length == 0) {
+        if (!userData.lastName.length) {
             lastNameError = "First Name required";
         } else {
             lastNameError = null;
         }
-        this.setState({ lastNameError });
+        setErrors({ ...errors, ...{ lastNameError } });
     }
 
-    onPhoneBlur() {
-        const phone = this.state.phone.replace(/\D/g, "");
+    const onPhoneBlur = () => {
+        const phone = userData.phone.replace(/\D/g, "");
         let phoneError = "";
-        if (phone.length == 0) {
+        if (!phone.length) {
             phoneError = "Phone required";
         } else if (phone.length !== 10) {
             phoneError = "Phone invalid";
         } else {
             phoneError = "";
         }
-        this.setState({ phoneError });
+        setErrors({ ...errors, ...{ phoneError } });
     }
 
-    onLastNameBlur() {
-        let lastNameError = null;
-        if (this.state.lastName.length == 0) {
-            lastNameError = "Last Name required";
-        } else {
-            lastNameError = null;
-        }
-        this.setState({ lastNameError });
-    }
-
-    onEmailBlur() {
+    const onEmailBlur = () => {
         let emailError = null;
-        if (this.state.email.length != 0 && !this.validateEmail(this.state.email)) {
+        if (!!userData.email.length && !validateEmail(userData.email)) {
             emailError = "Invalid Email";
         } else {
             emailError = null;
         }
-        this.setState({ emailError });
+        setErrors({ ...errors, ...{ emailError } });
     }
 
-    validateEmail(email) {
+    const validateEmail = (email) => {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     }
 
-    updateUser() {
-        const { email, phone, firstName, lastName } = this.state;
+    const updateUser = () => {
+        const { email, phone, firstName, lastName } = userData;
         const newUser = { email, phone, firstName, lastName };
         api
             .updateUser(newUser)
             .then(success => success.data)
             .then(success => {
-                this.setData(success);
-                this.editPressed();
+                setData(success);
+                editPressed();
             })
             .catch(() => {
                 Alert.alert("Problem", "Could not update.");
             });
     }
 
-    render() {
-        const { editable, user } = this.state;
-        return (
-            <>
-                {user &&
-                    <KeyboardAwareScrollView style={globalStyles.mainContainer} viewIsInsideTabBar>
-                        <Input
-                            label={"First Name"}
-                            ref={this.firstName}
-                            editable={this.state.editable}
-                            containerStyle={styles.field}
-                            value={this.state.firstName}
-                            errorStyle={ErrorStyle.style}
-                            errorMessage={this.state.firstNameError}
-                            onChangeText={this.onChange('firstName')}
-                            onBlur={() => this.onFirstNameBlur()}
-                            leftIcon={<Icon type="material" name="person" />}
-                        />
-                        <Input
-                            label={"Last Name"}
-                            ref={this.firstName}
-                            value={this.state.lastName}
-                            editable={this.state.editable}
-                            containerStyle={styles.field}
-                            errorStyle={ErrorStyle.style}
-                            errorMessage={this.state.lastNameError}
-                            onChangeText={this.onChange('lastName')}
-                            onBlur={() => this.onLastNameBlur()}
-                            leftIcon={<Icon type="material" name="person" />}
-                        />
-                        <Input
-                            label={"Phone"}
-                            keyboardType={"phone-pad"}
-                            editable={this.state.editable}
-                            value={this.state.phone}
-                            ref={this.phone}
-                            containerStyle={styles.field}
-                            errorStyle={ErrorStyle.style}
-                            errorMessage={this.state.phoneError}
-                            onChangeText={this.onChange('phone')}
-                            onBlur={() => this.onPhoneBlur()}
-                            leftIcon={<Icon type="material-community" name="phone" />}
-                        />
-                        <Input
-                            label={"Email"}
-                            autoCapitalize={"none"}
-                            keyboardType="email-address"
-                            editable={this.state.editable}
-                            ref={this.email}
-                            value={this.state.email}
-                            containerStyle={styles.field}
-                            errorStyle={ErrorStyle.style}
-                            errorMessage={this.state.emailError}
-                            onChangeText={this.onChange('email')}
-                            onBlur={() => this.onEmailBlur()}
-                            leftIcon={<Icon type="material-community" name="email" />}
-                        />
-                        <TouchableOpacity style={styles.logoutButton} onPress={this.logoutPressed}>
-                            <Text style={styles.logoutButtonText}>Logout</Text>
-                        </TouchableOpacity>
-                    </KeyboardAwareScrollView>
-                }
-                {
-                    editable &&
-                    <FloatingMainButton
-                        isEnabled
-                        onPress={() => this.updateUser()}
-                        title={'Save'}
-                        containerStyle={styles.saveButton}
+    React.useEffect(() => {
+        const user = auth.userObj;
+        if (!!user) {
+            setUserData(user);
+        } else {
+            logoutPressed();
+        }
+        navigation.setParams({
+            logoutPressed: logoutPressed,
+            editPressed: editPressed,
+            editText: "EDIT",
+        });
+    }, []);
+
+    return (
+        <>
+            {!!userData &&
+                <KeyboardAwareScrollView style={globalStyles.mainContainer} viewIsInsideTabBar>
+                    <Input
+                        label={"First Name"}
+                        editable={editable}
+                        containerStyle={styles.field}
+                        value={userData.firstName}
+                        errorStyle={ErrorStyle.style}
+                        errorMessage={errors.firstNameError}
+                        onChangeText={(value) => onChange('firstName', value)}
+                        onBlur={() => onFirstNameBlur()}
+                        leftIcon={<Icon type="material" name="person" />}
                     />
-                }
-            </>
-        );
-    }
+                    <Input
+                        label={"Last Name"}
+                        value={userData.lastName}
+                        editable={editable}
+                        containerStyle={styles.field}
+                        errorStyle={ErrorStyle.style}
+                        errorMessage={errors.lastNameError}
+                        onChangeText={(value) => onChange('lastName', value)}
+                        onBlur={() => onLastNameBlur()}
+                        leftIcon={<Icon type="material" name="person" />}
+                    />
+                    <Input
+                        label={"Phone"}
+                        keyboardType={"phone-pad"}
+                        editable={editable}
+                        value={userData.phone}
+                        containerStyle={styles.field}
+                        errorStyle={ErrorStyle.style}
+                        errorMessage={errors.phoneError}
+                        onChangeText={(value) => onChange('phone', value)}
+                        onBlur={() => onPhoneBlur()}
+                        leftIcon={<Icon type="material-community" name="phone" />}
+                    />
+                    <Input
+                        label={"Email"}
+                        autoCapitalize={"none"}
+                        keyboardType="email-address"
+                        editable={editable}
+                        value={userData.email}
+                        containerStyle={styles.field}
+                        errorStyle={ErrorStyle.style}
+                        errorMessage={errors.emailError}
+                        onChangeText={(value) => onChange('email', value)}
+                        onBlur={() => onEmailBlur()}
+                        leftIcon={<Icon type="material-community" name="email" />}
+                    />
+                    <TouchableOpacity style={styles.logoutButton} onPress={logoutPressed}>
+                        <Text style={styles.logoutButtonText}>Logout</Text>
+                    </TouchableOpacity>
+                </KeyboardAwareScrollView>
+            }
+            {
+                editable &&
+                <FloatingMainButton
+                    isEnabled
+                    onPress={() => updateUser()}
+                    title={'Save'}
+                    containerStyle={styles.saveButton}
+                />
+            }
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
