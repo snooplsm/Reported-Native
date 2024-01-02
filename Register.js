0b09e8941078aed65b3eec1efa1e764f12781238
night <night@durdom.com>
2024-01-01 19:17:19 -0500

    Register: convert to hooks

--- Register.js
+++ Register.js
@@ -18,10 +18,12 @@ import {
 } from "./Styles";
 import FloatingMainButton from "./FloatingMainButton";
 import { Badge, Button, CheckBox, Input, Icon } from "react-native-elements";
-import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useNavigation } from "@react-navigation/native";
+import { useAuth } from "./AuthProvider";
 
-export default class Register extends React.Component {
-  static navigationOptions = {
+export default function Register() {
+  const navigationOptions = {
     headerTitle: <LogoTitle title="REGISTER" />,
     headerRight: (
       <Icon
@@ -32,49 +34,37 @@ export default class Register extends React.Component {
     )
   };
 
-  constructor(props) {
-    super(props);
-    this.email = React.createRef();
-    this.state = {
-      email: "",
-      firstName: "",
-      lastName: "",
-      password: "",
-      phone: "",
-      error: false,
-      testify: false,
-      registering: false
-    };
+  const email = React.createRef();
+  const [stateEmail, setEmail] = React.useState('');
+  const [stateFirstName, setFirstName] = React.useState('');
+  const [stateLastName, setLastName] = React.useState('');
+  const [statePassword, setPassword] = React.useState('');
+  const [statePhone, setPhone] = React.useState('');
+  const [stateError, setError] = React.useState(false);
+  const [stateTestify, setTestify] = React.useState(false);
+  const [stateRegistering, setRegistering] = React.useState(false);
+  const [firstNameError, setFirstNameError] = React.useState();
+  const [lastNameError, setLastNameError] = React.useState();
+  const [phoneError, setPhoneError] = React.useState();
+  const [emailError, setEmailError] = React.useState();
+
+  const navigation = useNavigation();
+  const auth = useAuth();
+
+  const onAlreadyRegistered = () => {
+    navigation.replace("Login");
   }
 
-  onFieldChange(fieldName, val) {
-    this.setState({ [fieldName]: val })
+  const onFirstNameBlur = () => {
+    setFirstNameError(!stateFirstName.length ? "First Name required" : null);
   }
 
-  onAlreadyRegistered() {
-    this.props.navigation.replace("Login");
+  const onLastNameBlur = () => {
+    setLastNameError(!stateLastName.length ? "Last Name required" : null);
   }
 
-  onFirstNameBlur() {
-    const { firstName } = this.state;
-    this.setState({
-      firstNameError: firstName.length == 0 ? "First Name required" : null
-    });
-  }
-
-  onLastNameBlur() {
-    const { lastName } = this.state;
-    this.setState({
-      lastNameError: lastName.length == 0 ? "Last Name required" : null
-    });
-  }
-
-  onPhoneChange(phone) {
-    this.setState({ phone });
-  }
-
-  onPhoneBlur() {
-    const phone = this.state.phone.replace(/\D/g, "");
+  const onPhoneBlur = () => {
+    const phone = statePhone.replace(/\D/g, "");
     let phoneError = null;
     if (phone.length == 0) {
       phoneError = "Phone required";
@@ -83,192 +73,180 @@ export default class Register extends React.Component {
     } else {
       phoneError = "";
     }
-    this.setState({
-      phoneError
-    });
+    setPhoneError(phoneError);
   }
 
-  onEmailBlur(onlySuccess) {
+  const onEmailBlur = (onlySuccess) => {
     let emailError = null;
-    if (this.state.email.length != 0 && !this.validateEmail(this.state.email)) {
+    if (stateEmail.length != 0 && !validateEmail(stateEmail)) {
       emailError = "Invalid Email";
     }
 
-    if (this.state.emailError || !onlySuccess) {
-      this.setState({ emailError });
+    if (emailError || !onlySuccess) {
+      setEmailError(emailError);
     }
   }
 
-  validateEmail(email) {
+  const validateEmail = (email) => {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
 
-  submit() {
-    const { firstName, lastName, phone, testify, email, password } = this.state;
-    if (firstName.length < 1) {
+  const submit = () => {
+    if (!stateFirstName.length) {
       return Alert.alert(
         "First Name Required",
         "Name required to comply with 311 requirements."
       );
-    } else if (lastName.length < 1) {
+    } else if (!stateLastName.length) {
       return Alert.alert(
         "Last Name Required",
         "Name required to comply with 311 requirements."
       );
-    } else if (phone.replace(/\D/g, "").length != 10) {
+    } else if (statePhone.replace(/\D/g, "").length !== 10) {
       return Alert.alert(
         "Phone number invalid",
         "Ten digit phone number required."
       );
-    } else if (!this.validateEmail(email)) {
+    } else if (!validateEmail(stateEmail)) {
       return Alert.alert(
         "Email invalid",
         "Valid Email required to communicate with 311."
       );
-    } else if (!testify) {
+    } else if (!stateTestify) {
       return Alert.alert(
         "Testify required",
         "You must be willing to testify by phone to use Reported."
       );
     }
-    this.setState({ registering: true });
+    setRegistering(true);
 
     api
       .register({
-        firstName,
-        lastName,
-        phone: `(${phone.substring(0, 3)}) ${phone.substring(
+        firstName: stateFirstName,
+        lastName: stateLastName,
+        phone: `(${statePhone.substring(0, 3)}) ${statePhone.substring(
           3,
           6
-        )}-${phone.substring(6, 10)}`,
-        testify,
-        email,
-        password
+        )}-${statePhone.substring(6, 10)}`,
+        testify: stateTestify,
+        email: stateEmail,
+        password: statePassword,
       })
       .then(_success => {
-        const {
-          navigation: { navigate }
-        } = this.props;
-        this.setState({ registering: false });
-        navigate("Home");
+        console.log('register', _success);
+        setRegistering(false);
+        navigate('Home');
       })
       .catch(e => {
         const { response: res } = e;
-        this.setState({ registering: false });
+        setRegistering(false);
         const errorMessage = {
           401: "Credentials not found",
           422: "Could not process information"
         }[res.status] || "Unknown error";
-        this.setState({ error: errorMessage });
+        setError(errorMessage);
       });
   }
 
-  render() {
-    const { registering } = this.state;
-    return (
-      <SafeAreaView style={styles.mainWrapper}>
-        <KeyboardAwareScrollView style={globalStyles.mainContainer}>
-          <TouchableOpacity
-            style={styles.alreadyRegistered}
-            onPress={() => this.onAlreadyRegistered()}
-          >
-            <Text>Already Registered? Login</Text>
-          </TouchableOpacity>
-          <View
-            style={{
-              opacity: this.state.error ? 100 : 0,
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "row"
+  return (
+    <SafeAreaView style={styles.mainWrapper}>
+      <KeyboardAwareScrollView style={globalStyles.mainContainer}>
+        <TouchableOpacity
+          style={styles.alreadyRegistered}
+          onPress={() => onAlreadyRegistered()}
+        >
+          <Text>Already Registered? Login</Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            opacity: !!stateError ? 100 : 0,
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row"
+          }}
+        >
+          <Badge status="error" />
+          <Text> {!!stateError ?? "TAKE UP SPACE"}</Text>
+        </View>
+        <View style={{ marginTop: "10%" }} />
+        <View style={styles.inputWrapper}>
+          <Input
+            label="First Name"
+            containerStyle={styles.field}
+            errorStyle={ErrorStyle.style}
+            errorMessage={firstNameError}
+            onChangeText={v => setFirstName(v)}
+            onBlur={() => onFirstNameBlur()}
+            leftIcon={<Icon type="material" name="person" />}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Input
+            label="Last Name"
+            containerStyle={styles.field}
+            errorStyle={ErrorStyle.style}
+            errorMessage={lastNameError}
+            onChangeText={v => setLastName(v)}
+            onBlur={() => onLastNameBlur()}
+            leftIcon={<Icon type="material" name="person" />}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Input
+            label="Phone"
+            keyboardType="phone-pad"
+            containerStyle={styles.field}
+            errorStyle={ErrorStyle.style}
+            errorMessage={phoneError}
+            onChangeText={v => setPhone(v)}
+            onBlur={() => onPhoneBlur()}
+            leftIcon={<Icon type="material-community" name="phone" />}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Input
+            label="Email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            containerStyle={styles.field}
+            errorStyle={ErrorStyle.style}
+            errorMessage={emailError}
+            onChangeText={v => {
+              setEmail(v);
+              onEmailBlur(true);
             }}
-          >
-            <Badge status="error" />
-            <Text> {this.state.error ?? "TAKE UP SPACE"}</Text>
-          </View>
-          <View style={{ marginTop: "10%" }} />
-          <View style={styles.inputWrapper}>
-            <Input
-              label="First Name"
-              ref={this.firstName}
-              containerStyle={styles.field}
-              errorStyle={ErrorStyle.style}
-              errorMessage={this.state.firstNameError}
-              onChangeText={firstName => this.onFieldChange('firstName', firstName)}
-              onBlur={() => this.onFirstNameBlur()}
-              leftIcon={<Icon type="material" name="person" />}
-            />
-          </View>
-          <View style={styles.inputWrapper}>
-            <Input
-              label="Last Name"
-              ref={this.lastName}
-              containerStyle={styles.field}
-              errorStyle={ErrorStyle.style}
-              errorMessage={this.state.lastNameError}
-              onChangeText={lastName => this.onFieldChange('lastName', lastName)}
-              onBlur={() => this.onLastNameBlur()}
-              leftIcon={<Icon type="material" name="person" />}
-            />
-          </View>
-          <View style={styles.inputWrapper}>
-            <Input
-              label="Phone"
-              keyboardType="phone-pad"
-              ref={this.phone}
-              containerStyle={styles.field}
-              errorStyle={ErrorStyle.style}
-              errorMessage={this.state.phoneError}
-              onChangeText={email => this.onPhoneChange(email)}
-              onBlur={() => this.onPhoneBlur()}
-              leftIcon={<Icon type="material-community" name="phone" />}
-            />
-          </View>
-          <View style={styles.inputWrapper}>
-            <Input
-              label="Email"
-              autoCapitalize="none"
-              keyboardType="email-address"
-              ref={this.field}
-              containerStyle={styles.field}
-              errorStyle={ErrorStyle.style}
-              errorMessage={this.state.emailError}
-              onChangeText={email => {
-                this.onFieldChange('email', email);
-                this.onEmailBlur(true);
-              }}
-              onBlur={() => this.onEmailBlur(false)}
-              leftIcon={<Icon type="material-community" name="email" />}
-            />
-          </View>
-          <View style={styles.inputWrapper}>
-            <Input
-              label="Password (optional)"
-              secureTextEntry
-              containerStyle={styles.field}
-              onChangeText={password => this.setState({ password })}
-              errorStyle={ErrorStyle.style}
-              leftIcon={<Icon type="material-community" name="lock" />}
-            />
-          </View>
-          <View style={styles.inputWrapper}>
-            <CheckBox
-              containerStyle={styles.field}
-              onPress={() => this.setState({ testify: !this.state.testify })}
-              title={`I'm willing to testify at a hearing, which can be done by phone. I allow reported to use my images, locations, and descriptions publicly except when explicitly noted for private use.\n\nNote: The majority of complaints do not require a hearing. I understand that the information I submit in a report will be submitted to 311 via webform. I understand that my personal information is required to submit a complaint or compliment to 311.`}
-              checked={this.state.testify}
-            />
-          </View>
-        </KeyboardAwareScrollView>
-        <FloatingMainButton
-          isLoading={registering}
-          onPress={() => this.submit()}
-          title={'Register'}
-          containerStyle={styles.registerButton}
-        />
-      </SafeAreaView>
-    );
-  }
+            onBlur={() => onEmailBlur(false)}
+            leftIcon={<Icon type="material-community" name="email" />}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Input
+            label="Password (optional)"
+            secureTextEntry
+            containerStyle={styles.field}
+            onChangeText={v => setPassword(v)}
+            errorStyle={ErrorStyle.style}
+            leftIcon={<Icon type="material-community" name="lock" />}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <CheckBox
+            containerStyle={styles.field}
+            onPress={() => setTestify(!stateTestify)}
+            title={`I'm willing to testify at a hearing, which can be done by phone. I allow reported to use my images, locations, and descriptions publicly except when explicitly noted for private use.\n\nNote: The majority of complaints do not require a hearing. I understand that the information I submit in a report will be submitted to 311 via webform. I understand that my personal information is required to submit a complaint or compliment to 311.`}
+            checked={stateTestify}
+          />
+        </View>
+      </KeyboardAwareScrollView>
+      <FloatingMainButton
+        isLoading={stateRegistering}
+        onPress={() => submit()}
+        title={'Register'}
+        containerStyle={styles.registerButton}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
