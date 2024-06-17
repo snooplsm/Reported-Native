ee52e436a336d133e8ff8b57475f9f6384f89f1b
nochkin <github@durdom.com>
2024-06-16 23:59:41 -0400

    Trying to enable firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { Platform } from 'react-native';
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
@@ -6,7 +7,7 @@ import { initializeApp } from 'firebase/app';
 // import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
 // import {...} from "firebase/storage";
-import { analytics } from 'firebase/analytics';
+import { getAnalytics } from 'firebase/analytics';
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -14,12 +15,15 @@ const firebaseConfig = {
     authDomain: 'reported1-app.firebaseapp.com',
     databaseURL: 'https://reported1-app.firebaseio.com',
     projectId: 'reported1-app',
-    storageBucket: 'reported1-app.appspot.com',
-    messagingSenderId: 'sender-id',
-    appId: 'com.reported1.android',
-    measurementId: 'G-measurement-id',
+    // storageBucket: 'reported1-app.appspot.com',
+    // messagingSenderId: 'sender-id',
+    appId: Platform.OS === 'android' ? 'com.reported1.android' : 'com.reported1.ios',
+    // measurementId: 'G-measurement-id',
 };
 
-const app = initializeApp(firebaseConfig);
+const Firebase = initializeApp(firebaseConfig);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+const analytics = getAnalytics();
+
+export default analytics;
